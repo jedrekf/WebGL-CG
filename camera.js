663af43740822d7d3ccdd9c3f6bcfdd18030152c
jedrek <jedrek@fijalkowscy.com>
2017-11-29 19:51:29 +0100

    major refactoring

--- camera.js
+++ camera.js
@@ -3,9 +3,15 @@ var Camera = function(){
 	this.eye = [0, 0, -1]; // where are we
     this.center = [0, 0, 0]; // point we look at
     this.up = [0, 1, 0]; //vec3 pointing up
-    this.cameraVec;
+    this.cameraMatrix;
 
-	this.bindEvents = function(){
+	this.init = function(){
+        //prepare camera
+        this.cameraMatrix = new Float32Array(16);
+        mat4.lookAt(this.cameraMatrix, this.eye, this.center, this.up);
+       
+
+        //bind events
     	var self = this;
         canvas.onmousedown = handleMouseDown;
         document.onmouseup = handleMouseUp;
@@ -20,27 +26,27 @@ var Camera = function(){
             speed = performance.now() / 20000;
 
             switch (event.key) {
-                case "s":
-                // code for "s" key press.
-                    self.eye[1] += speed;
-                break;
                 case "w":
+                // code for "s" key press.
+                    mat4.translate(self.cameraMatrix, self.cameraMatrix,[0, 0, -speed]);
+                break;
+                case "s":
                 // code for "w" key press.
-                    self.eye[1] -= speed;
+                    mat4.translate(self.cameraMatrix, self.cameraMatrix, [0, 0, speed]);
                 break;
                 case "a":
                 // code for "a" key press.
-                    self.eye[0] += speed;
+                     mat4.translate(self.cameraMatrix, self.cameraMatrix, [-speed, 0, 0]);
                 break;
                 case "d":
                 // code for "d" key press.
-                    self.eye[0] -= speed;
+                     mat4.translate(self.cameraMatrix, self.cameraMatrix, [speed, 0, 0]);
                 break;
                 case "q": //sink - q
-                    self.eye[2] -= speed;
+                     mat4.translate(self.cameraMatrix, self.cameraMatrix, [0, -speed, 0]);
                 break;
                 case "e": //lift - e
-                    self.eye[2] += speed;
+                     mat4.translate(self.cameraMatrix, self.cameraMatrix, [0, speed, 0]);
                 break;
                 default:
                 return; // Quit when this doesn't handle the key event.
@@ -78,8 +84,16 @@ var Camera = function(){
         var yDelta = deltaY / 10;
 
         //vec3.set(eye, eye[0] - xDelta , eye[1] + yDelta, eye[2]);
-        vec3.set(self.center, self.center[0] + xDelta, self.center[1] + yDelta , 0);
+        //vec3.set(self.center, self.center[0] + xDelta, self.center[1] + yDelta , 0);
+        //mat4.rotate(self.cameraMatrix, self.cameraMatrix, degToRad(2), [yDelta, xDelta, 0]);
 
+        //have to use some trigonometric sin cos here?
+        var c = Math.cos(degToRad(yDelta));
+        var s = Math.sin(degToRad(xDelta));
+
+        mat4.rotateY(self.cameraMatrix, self.cameraMatrix, c);
+        mat4.rotateX(self.cameraMatrix, self.cameraMatrix, s);
+        
         lastMouseX = newX;
         lastMouseY = newY;
     };
